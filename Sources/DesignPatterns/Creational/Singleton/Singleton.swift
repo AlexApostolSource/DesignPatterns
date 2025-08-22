671fbe50bc63a18c2fa3811106a05deaa3589d51
AlexApostol <apostol516@gmail.com>
2025-08-22 16:18:04 +0200

    singleton

---
+++ Sources/DesignPatterns/Creational/Singleton/Singleton.swift
@@ -0,0 +1,17 @@
+//
+//  Singleton.swift
+//  DesignPatterns
+//
+//  Created by Alex.personal on 22/8/25.
+//
+
+import Foundation
+
+
+//final class MySingleton {
+//    private init() {}
+//    public static let shared = MySingleton()
+//}
+
+
+
